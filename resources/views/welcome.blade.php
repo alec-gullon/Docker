4da4ddf795b1b2cc7e289258d0eeaf5139ccd82e
Gullon, Alec <me@alecgullon.co.uk>
2020-03-18 15:29:08 +0000

    Testing a change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel (With a Change!)
                 </div>
 
                 <div class="links">
